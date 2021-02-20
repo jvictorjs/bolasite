c8c7935fb8f6c7a8ecd6b5ca8d1e3a48cf8778ce
jvictorhub <victorjtube@gmail.com>
2021-02-20 17:45:36 -0300

    add responsive sidenav and css new colors

--- frontend/src/app/components/template/nav/nav.component.ts
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { HeaderService } from './nav.service'
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({  // DECORATOR é o @
   selector: 'app-nav',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  events: string[] = [];
+  opened: boolean = true;
 
   ngOnInit(): void {
+    if (this.mobileQuery.matches) {
+      this.opened = false;
+    }
   }
 
+
+
+
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  constructor(private HeaderService: HeaderService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 800px)');
+
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+
+  ifMobileAndClosesSideNav(): void {
+    if (this.mobileQuery.matches) {
+      this.opened = false;
+    }
+    console.log('clicou ')
+    console.log('this.mobileQuery.matches = ' + this.mobileQuery.matches)
+    console.log('this.opened = ' + this.opened)
+  }
+
+
+  get title(): string {
+    if (this.mobileQuery.matches) {
+      this.opened = false;
+    } else {
+      this.opened = true;
+    }
+    return this.HeaderService.headerData.title
+  }
+
+  get icon(): string {
+    return this.HeaderService.headerData.icon
+  }
+
+  get routeUrl(): string {
+    return this.HeaderService.headerData.routeUrl
+  }
 }
